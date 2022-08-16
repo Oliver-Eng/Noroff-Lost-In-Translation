02a1fb669406554aff1d8017f69f66daa0f37943
Alexander <sheepsaurus@gmail.coim>
2022-08-16 14:51:11 +0200

    Initial Pages + Routing setup

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Profile = () => {
+
+    return (
+        <Link to="/">TO HOME</Link>
+    )
+}
+
+export default Profile;
---
+++ src/components/Translation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Translation = () => {
+
+    return (
+        <Link to="/profile">TO PROFILE</Link>
+    )
+}
+
+export default Translation;
