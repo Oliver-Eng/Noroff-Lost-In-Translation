d9aeaa730ba01a2bc5ac90a6c1a62497e3b8f55d
Alexander <sheepsaurus@gmail.coim>
2022-08-17 13:11:02 +0200

    Context addition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import React, { createContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home"
-import Translation from "./components/Translation"
-import Profile from "./components/Profile"
+import Home from "./components/Home";
+import Translation from "./components/Translation";
+import Profile from "./components/Profile";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/translation" element={<Translation />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const [state, setState] = useState("")
+    const MainContext = createContext(null);
+
+    return (
+        <MainContext.Provider value={ {state, setState} }>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/translation" element={<Translation />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </BrowserRouter>
+        </MainContext.Provider>
+    );
 }
 
 export default App;
