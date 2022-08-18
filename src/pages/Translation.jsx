b07d932d8695e837c6dba65936c77116427fa7cf
Alexander <sheepsaurus@gmail.coim>
2022-08-18 14:15:39 +0200

    Refactored Translation to component

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -1,52 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import withAuth from '../hoc/withAuth';
-import { addTranslation } from './../sources/user';
-import { useUser } from '../context/UserContext';
+import { TranslationSection } from "../components/Translation/TranslationSection" 
 
 const Translation = () => {
-	const [searchString, setSearchString] = useState("")
-	const [characters, setCharacters] = useState([])
-	const { user } = useUser();
-
-
-	const handleChange = (event) => {
-		setSearchString(event.target.value)
-	}
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-
-		let strippedSearch = searchString.replace(/\W/g, '') // Remove all non-alphanumeric characters
-
-		let newCharacters = Array.from(strippedSearch)
-			.map(char => char.toLowerCase())
-
-		fetch("https://obe-noroff-api.herokuapp.com/translations/" + user.id)
-			.then(res => res.json())
-			.then(json => {
-				let old = json.translations
-				old.push(searchString)
-
-				addTranslation(user.id, old)
-				setCharacters(newCharacters)
-			})
-		
-	}
 
 	return (
-		<React.Fragment>
-			<form onSubmit={handleSubmit}>
-				<input maxLength={40} onChange={handleChange}></input>
-				<button>Translate</button>
-			</form>
-			<div>
-				{characters.map((char, index) => {
-					return <img key={index} width="75px" src={"/images/" + char + ".png"} alt="sign"/>
-				})}
-			</div>
-		</React.Fragment>
-
-	);
+		<>
+			<TranslationSection />
+		</>
+	)
 };
 
 export default withAuth(Translation);
