8b6d70abe3b911910abd5076253d5c242ed1b735
Alexander <sheepsaurus@gmail.coim>
2022-08-19 12:58:06 +0200

    Simplistic implementation of Translation styling

--- src/pages/Translation.jsx
+++ src/pages/Translation.jsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import withAuth from '../hoc/withAuth';
 import { TranslationSection } from "../components/Translation/TranslationSection" 
+import Top from '../components/Home/Top/Top';
+import '../components/Home/TitleCard/TitleCard.css';
+import '../components/Home/LoginForm/LoginForm.css';
+import '../components/Home/Section/Section.css';
+import { addTranslation } from './../sources/user';
+import { useUser } from './../context/UserContext';
 
-const Translation = () => {
+const Translation = () => {    
+	const [searchString, setSearchString] = useState("")
+	const [characters, setCharacters] = useState([])
+	const { user } = useUser();
+
+
+	const handleChange = (event) => {
+		setSearchString(event.target.value)
+	}
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		let strippedSearch = searchString.replace(/\W/g, '') // Remove all non-alphanumeric characters
+
+		let newCharacters = Array.from(strippedSearch)
+			.map(char => char.toLowerCase())
+
+		fetch("https://obe-noroff-api.herokuapp.com/translations/" + user.id)
+			.then(res => res.json())
+			.then(json => {
+				let old = json.translations
+				old.push(searchString)
+
+				addTranslation(user.id, old)
+				setCharacters(newCharacters)
+			})
+		
+	}
 
 	return (
-		<>
-			<TranslationSection />
-		</>
+		<React.Fragment>
+			<Top />
+			
+			<div className='content-container-translation'>
+				{
+					<React.Fragment>
+						<div className='title-image-container'>
+							<form onSubmit={handleSubmit}>								
+								<input 
+									maxLength={40} 
+									onChange={handleChange} 
+									className='login-input-translation'
+									type='text'
+									placeholder="Insert text"
+								/>{' '}
+								<img id='keyboard-image-translation' src='/images/logo-graphics/Keyboard.png' alt='keyboard'></img>
+								
+								<button className='submit-button-translation' type='submit'>
+									<img id='button-image' src='/images/logo-graphics/Right_Arrow.png' alt='right_arrow'></img>
+								</button>							
+							</form>
+						</div>
+						
+						<div className='form-box-translation'>							
+							{characters.map((char, index) => {
+								return <img key={index} width="75px" src={"/images/hand-signs/" + char + ".png"} alt="sign"/>
+							})}
+						</div>
+					</React.Fragment>
+				}
+			</div>
+			<div>
+			</div>
+		</React.Fragment>
 	)
 };
 
