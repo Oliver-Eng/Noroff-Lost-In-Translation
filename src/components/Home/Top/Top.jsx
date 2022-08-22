a24ef61e96229d9514f064485dd94a210d4b3bc1
Alexander <sheepsaurus@gmail.coim>
2022-08-22 12:23:27 +0200

    Top profile icon + navigation

--- src/components/Home/Top/Top.jsx
+++ src/components/Home/Top/Top.jsx
@@ -1,10 +1,25 @@
 import './Top.css';
+import { useUser } from '../../../context/UserContext';
+import { useNavigate } from 'react-router';
 
 const Top = () => {
+	const { user, setUser } = useUser();
+	const navigate = useNavigate();
+
+	const profileNavigate = () => {		
+		navigate("/profile")
+	}
+
 	return (
 		<div className='top-bar'>
 			<div className='top-bar-text-container'>
 				<p>Lost in Translation</p>
+			</div>
+			<div onClick={profileNavigate} className='top-bar-profilename-container'>				
+				<div>{user.username}</div>
+			</div>
+			<div onClick={profileNavigate} className='top-bar-profile-container'>
+				<img src="/images/logo-graphics/Profile.png" alt="profileimage"></img>
 			</div>
 		</div>
 	);
