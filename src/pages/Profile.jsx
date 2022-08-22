c7eb08bd6ed53edf8fc62203a712b0c15cedb159
OliverEng <Oliver@Engermann.dk>
2022-08-22 19:10:37 +0200

    Moved 'boot' functionality to withAuth
    Also added a translation emblem like in assignment PDF.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,37 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import withAuth from '../hoc/withAuth';
 import Top from '../components/Top/Top';
 import ProfileSection from '../components/Profile/ProfileSection';
-import { STORAGE_KEY_USER } from '../const/storageKeys';
-import { useUser } from '../context/UserContext';
-
 
 const Profile = () => {
-	const { user } = useUser()
-
-	useEffect(() => {		
-		// async function fetchUser() {
-		// 	const [error, data] = await checkForUser(user.username);
-		// 	console.log(data)
-			
-		// 	if (data.length == 0) {
-		// 		localStorage.removeItem(STORAGE_KEY_USER);
-		// 		navigate('/');
-		// 	}
-		// }
-
-		// fetchUser();
-
-		// Get user from API, check if it is empty, push to login if it is.
-		fetch('https://obe-noroff-api.herokuapp.com/translations/' + user.id)
-			.then((res) => {
-				if (res.status == 404) {					
-					localStorage.removeItem(STORAGE_KEY_USER);
-					window.location = "/"
-				}
-			})
-	}, []);
-
 	return (
 		<React.Fragment>
 			<Top />
