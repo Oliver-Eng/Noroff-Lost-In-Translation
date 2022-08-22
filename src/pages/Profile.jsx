0b6aeab1582ccba6d4d7c8904c4ed52f3c0686ba
Alexander <sheepsaurus@gmail.coim>
2022-08-22 18:12:56 +0200

    initial

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,18 +10,6 @@ const Profile = () => {
 	const { user } = useUser()
 
 	useEffect(() => {		
-		// async function fetchUser() {
-		// 	const [error, data] = await checkForUser(user.username);
-		// 	console.log(data)
-			
-		// 	if (data.length == 0) {
-		// 		localStorage.removeItem(STORAGE_KEY_USER);
-		// 		navigate('/');
-		// 	}
-		// }
-
-		// fetchUser();
-
 		// Get user from API, check if it is empty, push to login if it is.
 		fetch('https://obe-noroff-api.herokuapp.com/translations/' + user.id)
 			.then((res) => {
