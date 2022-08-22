aa41ed6749445e8c97b1d7b2b4b5a2718ddad504
Alexander <sheepsaurus@gmail.coim>
2022-08-22 13:42:59 +0200

    Remove MainContext

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import MainContext from './context/MainContext';
+import UserProvider from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<MainContext>
+			<UserProvider>
 				<App />
-			</MainContext>
+			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
